c3cb65b9367bd5852f556ccaa301267e8f1f59f5
Vishalkumarjha72 <110764035+Vishalkumarjha72@users.noreply.github.com>
2023-08-06 17:51:16 +0530

    feat: bottom action sheet UI done Vishalkumarjha72

---
+++ lib/features/recommendation/presentation/widgets/ingredient_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:secret_spice/core/constants/theme/colors.dart';
+import 'package:secret_spice/core/constants/theme/typography.dart';
+
+
+class IngredientList extends StatelessWidget {
+  final List<String> ingredients;
+  
+  const IngredientList({
+    super.key, required this.ingredients,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 114,
+      child: ScrollConfiguration(
+        behavior: const MaterialScrollBehavior().copyWith(overscroll: false),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemExtent: 122,
+          itemCount: ingredients.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.fromLTRB(2, 20, 20, 20),
+              decoration: BoxDecoration(
+                  color: white,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: const Color(0XFF0C1A4B).withOpacity(0.05),
+                      blurRadius: 1,
+                    ),
+                    BoxShadow(
+                      color: neutral800.withOpacity(0.075),
+                      blurRadius: 20,
+                      spreadRadius: -2,
+                      offset: const Offset(0, 4),
+                    ),
+                  ]),
+              child: Center(
+                child: Text(
+                  ingredients[index],
+                  style: body2.copyWith(color: neutral500),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
