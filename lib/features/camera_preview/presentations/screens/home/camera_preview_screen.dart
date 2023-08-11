1a4d2eb9c422cbe7d1249286a7f23cab66dc44fc
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2023-08-11 23:47:00 +0530

    feat: camera preview ui

--- lib/features/camera_preview/presentations/screens/home/camera_preview_screen.dart
+++ lib/features/camera_preview/presentations/screens/home/camera_preview_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -18,6 +19,8 @@ class CameraPreviewScreen extends StatefulWidget {
 
 class _CameraPreviewScreen extends State<CameraPreviewScreen> {
   late CameraController cameraController;
+  late XFile image;
+  FlashMode _flashMode = FlashMode.off;
 
   @override
   void initState() {
@@ -53,130 +56,149 @@ class _CameraPreviewScreen extends State<CameraPreviewScreen> {
     if (!cameraController.value.isInitialized) {
       return Scaffold(
         backgroundColor: white,
-        body: Center(
-          
-        ),
+        body: const Center(),
       );
     }
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          SizedBox(
+      body: Stack(children: <Widget>[
+        SizedBox(
             height: double.maxFinite,
             width: double.maxFinite,
-            child: CameraPreview(cameraController)
-          ),
-          SafeArea(
-            minimum: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Stack(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    "Secret Spices",
-                    style: heading4.copyWith(color: white),
-                  ),
+            child: CameraPreview(cameraController)),
+        SafeArea(
+          minimum: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  "Secret Spices",
+                  style: heading4.copyWith(color: white),
                 ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: GestureDetector(
-                    onTap: (){},
-                    child: SvgPicture.asset(
-                      "assets/icons/FlashOnIcon.svg",
-                      width: 24,
-                      height: 24,
-                    )
-                  ),
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: GestureDetector(
+                    onTap: () {
+                      if (_flashMode == FlashMode.off) {
+                        cameraController.setFlashMode(FlashMode.torch);
+                        setState(() {
+                          _flashMode = FlashMode.torch;
+                        });
+                      } else {
+                        cameraController.setFlashMode(FlashMode.off);
+                        setState(() {
+                          _flashMode = FlashMode.off;
+                        });
+                      }
+                    },
+                    child: _flashMode == FlashMode.off
+                        ? SvgPicture.asset(
+                            "assets/icons/FlashOnIcon.svg",
+                            width: 24,
+                            height: 24,
+                          )
+                        : SvgPicture.asset(
+                            "assets/icons/FlashOffIcon.svg",
+                            width: 24,
+                            height: 24,
+                          )),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: SvgPicture.asset(
+                  "assets/icons/ScanIcon.svg",
+                  width: min(MediaQuery.of(context).size.width * 0.9, 512),
+                  height: min(MediaQuery.of(context).size.width * 0.9, 512),
                 ),
-                Align(
-                  alignment: Alignment.center,
-                  child: SvgPicture.asset(
-                    "assets/icons/ScanIcon.svg",
-                    width: min(MediaQuery.of(context).size.width, 512),
-                    height: min(MediaQuery.of(context).size.width, 512),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 12),
-                        decoration: BoxDecoration(
-                          color: neutral900.withOpacity(0.4),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Text(
-                          "Tap the shutter button to search",
-                          style: body1.copyWith(color: white),
-                        ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 12),
+                      decoration: BoxDecoration(
+                        color: neutral900.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(16),
                       ),
-                      const SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: (){},
+                      child: Text(
+                        "Tap the shutter button to search",
+                        style: body1.copyWith(color: white),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () async {
+                            try {
+                              setState(() async {
+                                image = await cameraController.takePicture();
+                              });
+                              if (kDebugMode) {
+                                print(image.path);
+                              }
+                            } catch (e) {}
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(
+                                width: 1,
+                                color: white,
+                              ),
+                              borderRadius: BorderRadius.circular(999),
+                            ),
                             child: Container(
-                              padding: const EdgeInsets.all(2),
+                              padding: const EdgeInsets.all(16),
+                              margin: const EdgeInsets.all(2),
                               decoration: BoxDecoration(
-                                color: Colors.transparent,
-                                border: Border.all(
-                                  width: 1,
-                                  color: white,
-                                ),
+                                color: white,
                                 borderRadius: BorderRadius.circular(999),
                               ),
-                              child: Container(
-                                padding: const EdgeInsets.all(16),
-                                margin: const EdgeInsets.all(2),
-                                decoration: BoxDecoration(
-                                  color: white,
-                                  borderRadius: BorderRadius.circular(999),
-                                ),
-                                child: SvgPicture.asset(
-                                  "assets/icons/SearchIcon.svg",
-                                  width: 32,
-                                  height: 32,
-                                ),
+                              child: SvgPicture.asset(
+                                "assets/icons/SearchIcon.svg",
+                                width: 32,
+                                height: 32,
                               ),
                             ),
                           ),
-                          Container(
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              left: BorderSide(width: 1, color: white),
+                              top: BorderSide(width: 1, color: white),
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(16),
+                            ),
+                          ),
+                          child: Container(
                             decoration: BoxDecoration(
-                              border: Border(
-                                left: BorderSide(
-                                  width: 1,
-                                  color: white
-                                ),
-                                top: BorderSide(
-                                  width: 1,
-                                  color: white
-                                ),
-                              ),
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(16),
-                              ),
+                              color: white,
                             ),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: white,
-
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 10,),
-                    ],
-                  ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ]
-      ),
+        ),
+      ]),
     );
   }
 }
